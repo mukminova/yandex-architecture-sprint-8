859d9670304dfc5a4f5714b2e259161331756a5b
mukminova <vjzkbb@gmail.com>
2025-01-13 08:40:21 +0300

    Task 8

---
+++ backend/src/main/java/ru/yandex/practicum/report/controller/ReportController.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.report.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/reports")
+public class ReportController {
+
+    @GetMapping
+    public ResponseEntity<String> getReports() {
+        return ResponseEntity.ok("reports");
+    }
+
+}
